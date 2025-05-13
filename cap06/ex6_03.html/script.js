f8bfaa2b238a9bf105faec623bbf88bbbea17543
Lucioafk <luciogabriel1550@gmail.com>
2025-05-13 16:48:54 -0300

    atualização

--- cap06/ex6_03.html/script.js
+++ cap06/ex6_03.html/script.js
@@ -6,7 +6,7 @@ const carros = []
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const modelo = frm.inModelo.value
-    const preco = Number(frm.inPreco.valor)
+    const preco = Number(frm.inPreco.value)
 
     carros.push({modelo, preco})
     frm.inModelo.value = ""
@@ -16,3 +16,43 @@ frm.addEventListener("submit", (e) => {
 
     frm.btListar.dispatchEvent(new Event("click"))
 })
+
+frm.btListar.addEventListener("click", () => {
+    if(carros.length == 0) {
+        alert("Não há carros na lista")
+        return
+    }
+
+    const lista = carros.reduce((acumulador, carro) => acumulador + carro.modelo + "- R$: " + carro.preco.toFixed(2) + "\n","")
+    resp.innerText = `Lista dos Carros Cadastrados \n ${"-".repeat(40)} \n ${lista}`
+
+
+})
+
+ frm.btListar.addEventListener("click", () => {  // "escuta" clique em btListar 
+  if (carros.length == 0) {                     // se tamanho do vetor é igual a 0
+    alert("Não há carros na lista")
+    return
+  }
+  // método reduce() concatena uma string, obtendo modelo e preço de cada veículo
+  const lista = carros.reduce((acumulador, carro) =>
+    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+  resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
+ })
+
+
+frm.btSimular.addEventListener("click", () => {
+  const desconto = Number(prompt("Qual o percentual de desconto: "))
+  if (desconto == 0 || isNaN(desconto)) {  
+    return                            
+  }
+  const carrosDesc = carros.map(aux => ({
+    modelo: aux.modelo,
+    preco: aux.preco - (aux.preco * desconto / 100)
+  }))
+  let lista = ""
+  for (const carro of carrosDesc) {   
+    lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+  }
+  resp.innerText = `Carros com desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
+ })
