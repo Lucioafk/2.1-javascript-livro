03893b69ddddf5749ad778e289a0a7d1102dc23e
Lucioafk <luciogabriel1550@gmail.com>
2025-05-21 08:16:07 -0300

    criptografia

---
+++ cap07/ex7_12.js/script.js
@@ -0,0 +1,34 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h2")
+const resetar = document.querySelector("button")
+
+
+
+frm.addEventListener("submit", (e) => {
+e.preventDefault()
+let texto = frm.inTexto.value
+let par = ""
+let impar = ""
+
+for (let i = 0; i < texto.length; i++) {
+  if(i % 2 === 0){
+    par += texto.charAt(i)
+  } else {
+    impar += texto.charAt(i)
+  }
+}
+
+resp.innerText = `${impar + par}`
+
+
+})
+
+frm.outDescriptografar.addEventListener("click", () => {
+    let texto = frm.inTexto.value
+    resp.innerText = `${texto}`
+
+})
+
+
+
+
